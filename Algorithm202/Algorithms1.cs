27357af1de0bb49a113d8d0b2ec61ba4372fb08a
David Ogwuche <victorochez@outlook.com>
2023-05-23 01:10:01 +0100

    Initial

--- Algorithm202/Algorithms1.cs
+++ Algorithm202/Algorithms1.cs
@@ -47,5 +47,51 @@
             return new List<int> { (n + 50) / 100 - (1), 1, (n + 50) % 100 / 20 };
         }
     }
+    /*
+     Task
+    Finish the function numberToOrdinal, which should take a number and return it as a string with the correct ordinal indicator suffix (in English). For example, 1 turns into "1st".
 
+    For the purposes of this challenge, you may assume that the function will always be passed a non-negative integer. If the function is given 0 as an argument, it should return the string "0" without a suffix.
+
+    To help you get started, here is an excerpt from Wikipedia's page on Ordinal Indicators:
+
+    st is used with numbers ending in 1 (e.g. 1st, pronounced first)
+    nd is used with numbers ending in 2 (e.g. 92nd, pronounced ninety-second)
+    rd is used with numbers ending in 3 (e.g. 33rd, pronounced thirty-third)
+    As an exception to the above rules, all the "teen" numbers ending with 11, 12 or 13 use -th (e.g. 11th, pronounced eleventh, 112th, pronounced one hundred [and] twelfth)
+    th is used for all other numbers (e.g. 9th, pronounced ninth).
+    Specification
+    Challenge.NumberToOrdinal(number)
+    take a number and return it as a string with the correct English ordinal indicator suffix
+     */
+    public static string ToOrdinal(int number)
+    {
+        switch (number % 10)
+        {
+            case 11:
+            case 12:
+            case 13:
+                return number + "th";
+        }
+        if (number <= 0)
+        {
+            return "0";
+        }
+        else if (number.ToString().EndsWith("1"))
+        {
+            return number + "st";
+        }
+        else if (number.ToString().EndsWith("2"))
+        {
+            return number + "nd";
+        }
+        else if (number.ToString().EndsWith("3"))
+        {
+            return number + "rd";
+        }
+        else
+        {
+            return number + "th";
+        }
+    }
 }
